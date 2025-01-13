57fb53262ce16b19280025224dffd2194842d5e5
iffn <iffn1990@gmail.com>
2025-01-13 22:31:56 +0100

    Added selector to paint tool

--- Source/EditorTools/SimpleClickToPaintTool.cs
+++ Source/EditorTools/SimpleClickToPaintTool.cs
@@ -6,57 +6,43 @@ using UnityEngine;
 
 public class SimpleClickToPaintTool : BaseTool
 {
-    EditShape selectedShape;
     bool raycastActive;
     Vector3 originalShapePosition;
     Color32 brushColor;
     AnimationCurve brushCurve = AnimationCurve.Linear(0, 0, 1, 1);
 
+    public PlaceableByClickHandler PlaceableByClick;
+
     public override string DisplayName => "Click to paint tool";
 
     public override void OnEnable()
     {
-        if (selectedShape) originalShapePosition = selectedShape.transform.position;
+        if (PlaceableByClick == null) PlaceableByClick = new PlaceableByClickHandler(LinkedMarchingCubeController);
     }
 
     public override void OnDisable()
     {
-        if (selectedShape) selectedShape.transform.position = originalShapePosition;
+        
     }
 
     public override void DrawUI()
     {
         // Handle shape assignment
-        EditShape newSelectedShape = EditorGUILayout.ObjectField(
-            "Brush Shape",
-            selectedShape,
-            typeof(EditShape),
-            true
-        ) as EditShape;
+        PlaceableByClick.EditorUI();
 
         // Handle color assignment
         brushColor = EditorGUILayout.ColorField("Brush Color", brushColor);
 
         // Handle curve assignment
         brushCurve = EditorGUILayout.CurveField("Brush Curve", brushCurve, Color.cyan, new Rect(0, 0, 1f, 1f));
-        
-        if(newSelectedShape && newSelectedShape != selectedShape)
-        {
-            RestoreShapePositionIfAble();
-            selectedShape = newSelectedShape;
-            SaveShapePositionIfAble();
-            newSelectedShape.Initialize();
-        }
 
-        if (!newSelectedShape) RestoreShapePositionIfAble();
-
-        if (!selectedShape) return;
+        if (PlaceableByClick == null) return;
 
         //Settings
         bool newRaycastActive = EditorGUILayout.Toggle("Active", raycastActive);
         if(raycastActive != newRaycastActive)
         {
-            selectedShape.gameObject.SetActive(false);
+            PlaceableByClick.SelectedEditShape.gameObject.SetActive(false);
             LinkedMarchingCubeController.DisplayPreviewShape = false;
             LinkedMarchingCubeController.EnableAllColliders = newRaycastActive;
             raycastActive = newRaycastActive;
@@ -78,51 +64,41 @@ public class SimpleClickToPaintTool : BaseTool
 
         if(result != RayHitResult.None)
         {
-            selectedShape.gameObject.SetActive(true);
-            selectedShape.transform.position = result.point;
+            PlaceableByClick.SelectedEditShape.gameObject.SetActive(true);
+            PlaceableByClick.SelectedEditShape.transform.position = result.point;
 
             HandleDirectUpdate(e);
         }
         else
         {
-            selectedShape.gameObject.SetActive(false);
+            PlaceableByClick.SelectedEditShape.gameObject.SetActive(false);
             LinkedMarchingCubeController.DisplayPreviewShape = false;
         }
 
-        if (e.shift && e.type == EventType.ScrollWheel)
+        if (PlaceableByClick.SelectedShape != null) PlaceableByClick.SelectedEditShape.HandleSceneUpdate(e);
+
+        if (EscapeDownEvent(e))
         {
-            float scaleDelta = e.delta.x * -0.03f; // Scale factor; reverse direction if needed
-
-            selectedShape.transform.localScale *= (scaleDelta + 1);
-
-            e.Use(); // Mark event as handled
-        }
-    }
-
-    void HandleDirectUpdate(Event e)
-    {
-        selectedShape.gameObject.SetActive(true);
-
-        // Left-click event
-        if (LeftClickDownEvent(e))
-        {
-            LinkedMarchingCubeController.ModificationManager.ModifyData(
-                selectedShape, 
-                new BaseModificationTools.ChangeColorModifier(brushColor, brushCurve)
-            );
+            raycastActive = false;
             e.Use();
             return;
         }
     }
 
-    void SaveShapePositionIfAble()
+    void HandleDirectUpdate(Event e)
     {
-        if (selectedShape) originalShapePosition = selectedShape.transform.position;
-    }
+        PlaceableByClick.SelectedEditShape.gameObject.SetActive(true);
 
-    void RestoreShapePositionIfAble()
-    {
-        if (selectedShape) selectedShape.transform.position = originalShapePosition;
+        // Left-click event
+        if (LeftClickDownEvent(e))
+        {
+            LinkedMarchingCubeController.ModificationManager.ModifyData(
+                PlaceableByClick.SelectedEditShape, 
+                new BaseModificationTools.ChangeColorModifier(brushColor, brushCurve)
+            );
+            e.Use();
+            return;
+        }
     }
 }
 
