3d3e5d940977d215b4e55bcad8bb668b28cb6e57
iffn <iffn1990@gmail.com>
2025-03-18 08:51:23 +0100

    Revert "Improved performance, upgrade broken"
    This reverts commit 49d4c9648c919f85a508fbabc42a88b7ee19b920.

--- Source/Core scripts/VoxelData.cs
+++ Source/Core scripts/VoxelData.cs
@@ -8,7 +8,7 @@ namespace iffnsStuff.MarchingCubeEditor.Core
     public struct VoxelData
     {
         public readonly static VoxelData Empty = new VoxelData(-1.0f, new Color32(255, 255, 255, 255));
-        public readonly static int Size = 8;
+        public readonly static int Size = 4 + 32;
 
         public VoxelData(float weight, Color32 color)
         {
@@ -41,17 +41,6 @@ namespace iffnsStuff.MarchingCubeEditor.Core
         {
             WeightInsideIsPositive = BitConverter.ToSingle(src, srcOffset);
             Color = new Color32(src[srcOffset + 4], src[srcOffset + 5], src[srcOffset + 6], src[srcOffset + 7]);
-        }
-
-        public byte[] SerializeCompressed()
-        {
-            byte[] data = new byte[5]; // 1 byte weight, 4 bytes color
-            data[0] = (byte)((WeightInsideIsPositive + 1f) * 127.5f); // Convert -1...1 to 0-255
-            data[1] = Color.r;
-            data[2] = Color.g;
-            data[3] = Color.b;
-            data[4] = Color.a;
-            return data;
         }
     }
 }
