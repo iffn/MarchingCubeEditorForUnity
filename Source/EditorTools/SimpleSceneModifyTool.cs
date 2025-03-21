37abdb9a2fc780328c738816f015739eed939f92
iffn <iffn1990@gmail.com>
2025-03-21 15:32:50 +0100

    Added button to move shape to object

--- Source/EditorTools/SimpleSceneModifyTool.cs
+++ Source/EditorTools/SimpleSceneModifyTool.cs
@@ -12,6 +12,10 @@ public class SimpleSceneModifyTool : BaseTool
 
     PlaceableByClickHandler placeableByClick;
 
+    bool movingActive = false;
+
+    static readonly Color highlightColor = new Color(0.7f, 0.7f, 1f); //ToDo: Improve highlight color
+
     public override void OnEnable()
     {
         if (placeableByClick == null) placeableByClick = new PlaceableByClickHandler(LinkedMarchingCubeController);
@@ -23,12 +27,59 @@ public class SimpleSceneModifyTool : BaseTool
 
         placeableByClick.DrawEditorUI();
 
+        // Store original colors
+        Color originalBackground = GUI.backgroundColor;
+        Color originalContentColor = GUI.contentColor;
+
+        if (movingActive)
+        {
+            // Set custom colors for the selected tool
+            GUI.backgroundColor = highlightColor;
+            GUI.contentColor = Color.white; // Text color
+
+            ShowClickToMoveToOriginButton();
+
+            // Restore original colors
+            GUI.backgroundColor = originalBackground;
+            GUI.contentColor = originalContentColor;
+        }
+        else
+        {
+            ShowClickToMoveToOriginButton();
+        }
+
+        // Show add and remove options
         EditorGUILayout.BeginHorizontal();
         if (GUILayout.Button($"Add {placeableByClick.SelectedEditShape.transform.name}"))
             LinkedMarchingCubeController.ModificationManager.ModifyData(placeableByClick.SelectedEditShape, new BaseModificationTools.AddShapeModifier());
         if (GUILayout.Button($"Subtract {placeableByClick.SelectedEditShape.transform.name}"))
             LinkedMarchingCubeController.ModificationManager.ModifyData(placeableByClick.SelectedEditShape, new BaseModificationTools.SubtractShapeModifier());
         EditorGUILayout.EndHorizontal();
+
+        void ShowClickToMoveToOriginButton()
+        {
+            if (GUILayout.Button($"Move to origin of click on object"))
+                movingActive = !movingActive;
+        }
+    }
+
+    public override void HandleSceneUpdate(Event currentEvent)
+    {
+        base.HandleSceneUpdate(currentEvent);
+
+        if(movingActive)
+        {
+            if (LeftClickDownEvent(currentEvent))
+            {
+                Ray ray = HandleUtility.GUIPointToWorldRay(currentEvent.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Transform hitObject = hit.transform;
+                    placeableByClick.SelectedEditShape.transform.SetPositionAndRotation(hitObject.position, hitObject.rotation);
+                }
+            }
+        }
     }
 }
 #endif
