a5e279350d93ce38485242a500755dee5107eb2c
iffn <iffn1990@gmail.com>
2025-07-19 12:30:44 +0200

    Reverted default chunk size

--- Source/Core scripts/MarchingCubesController.cs
+++ Source/Core scripts/MarchingCubesController.cs
@@ -29,7 +29,7 @@ namespace iffnsStuff.MarchingCubeEditor.Core
 
         MarchingCubesModel mainModel;
         MarchingCubesModel previewModelWithOldData;
-        static readonly Vector3Int defaultChunkSize = new Vector3Int(20, 20, 20);
+        static readonly Vector3Int defaultChunkSize = new Vector3Int(16, 16, 16);
         Vector3Int chunkSize = defaultChunkSize;
         readonly List<MarchingCubesView> chunkViews = new List<MarchingCubesView>();
 
